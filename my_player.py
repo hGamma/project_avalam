8734f3e5db4b81e7182a84b66e450f44fad3a662
Gaspard Cornu-Emieux <gaspard.ce3@gmail.com>
2022-10-21 17:05:24 -0400

    feat(ab) : Implemented iterative alpha beta with the CleanerBoard

--- my_player.py
+++ my_player.py
@@ -19,37 +19,25 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 """
 from avalam import *
 import math
+import time
 
-class CleanedBoard(Board):
-
-    max_height = 5
-    initial_board = [ [ 0,  0,  1, -1,  0,  0,  0,  0,  0],
-                      [ 0,  1, -1,  1, -1,  0,  0,  0,  0],
-                      [ 0, -1,  1, -1,  1, -1,  1,  0,  0],
-                      [ 0,  1, -1,  1, -1,  1, -1,  1, -1],
-                      [ 1, -1,  1, -1,  0, -1,  1, -1,  1],
-                      [-1,  1, -1,  1, -1,  1, -1,  1,  0],
-                      [ 0,  0,  1, -1,  1, -1,  1, -1,  0],
-                      [ 0,  0,  0,  0, -1,  1, -1,  1,  0],
-                      [ 0,  0,  0,  0,  0, -1,  1,  0,  0] ]
-
+class CleanedBoard():
     def __init__(self, board : Board):
-        super(self)
-        self.m = board.m
-        self.rows = board.rows
-        self.columns = board.columns
-        self.max_height = board.max_height
+        self._board = board
+        self._cleaned_m = board.m
+        self._garanteed_score = 0
 
         # Compute the heuristic of the board, i.e. the difference between the non-movable towers of the
         # player and the non-movable towers of its opponent.
-        self.score = board.get_score()
-        for (i,j,_) in board.get_towers:
+        for (i,j,value) in board.get_towers():
             if not board.is_tower_movable(i,j):
-                self.m[i][j] = 0
-    
-        self._rotation = lambda m : [[m[m.columns - j][i] for i in range(m.rows)] for j in range(m.columns)]
-        self._reflexion = lambda m : [[m[m.rows - i][j] for j in range(m.columns)] for i in range(m.rows)]
-        self._invert = lambda m : [[-m[i][j] for j in range(m.columns)] for i in range(m.rows)]
+                self._cleaned_m[i][j] = 0
+                self._garanteed_score += 1 if (value > 0) else -1
+
+        # Define the group of symetry functions
+        self._rotation = lambda m : [[m[board.columns - j][i] for i in range(board.rows)] for j in range(board.columns)]
+        self._reflexion = lambda m : [[m[board.rows - i][j] for j in range(board.columns)] for i in range(board.rows)]
+        self._invert = lambda m : [[-m[i][j] for j in range(board.columns)] for i in range(board.rows)]
 
         self._symetry_functions = [
             lambda m : m,
@@ -61,14 +49,31 @@ class CleanedBoard(Board):
             lambda m : self._reflexion(self._rotation(self._rotation(self._rotation(m))))
         ]
 
+    def get_cleaned_m(self):
+        return self._cleaned_m
+
     def are_similar(self, other):
-        return any([symetry(other.m) == self.m for symetry in self._symetry_functions])
+        return any([symetry(other.get_cleaned_m()) == self._cleaned_m for symetry in self._symetry_functions])
 
     def are_invert_similar(self, other):
-        return any([self._invert(symetry(other.m)) == self.m for symetry in self._symetry_functions])
+        return any([self._invert(symetry(other.get_cleaned_m())) == self._cleaned_m for symetry in self._symetry_functions])
     
+    def get_garanteed_score(self):
+        return self._garanteed_score
+
     def get_score(self):
-        return self.score
+        return self._board.get_score()
+    
+    def clone(self):
+        return CleanedBoard(self._board)
+
+    def is_finished(self):
+        return self._board.is_finished()
+    
+    def get_actions(self):
+        for i, j, h in self._board.get_towers():
+            for action in self._board.get_tower_actions(i, j):
+                yield action
 
 class MyAgent(Agent):
 
@@ -102,38 +107,37 @@ class MyAgent(Agent):
         :return: an action
             eg; (1, 4, 1 , 3) to move tower on cell (1,4) to cell (1,3)
         """
-        print("percept:", percepts)
-        print("player:", player)
         print("step:", step)
         print("time left:", time_left if time_left else '+inf')
 
         board = dict_to_board(percepts)
         board.get_percepts((player == -1))
+        cleaned_board = CleanedBoard(board)
 
-        action = self.h_alphabeta_search(board)[1]
+        action = self.h_alphabeta_search(cleaned_board, 5)
         print("Action played:", action)
         return action
 
     def h_alphabeta_search(
         self,
-        board,
-        cutoff=lambda board, depth: depth > 2,
-        heuristic=lambda board : board.get_score()
+        cleaned_board : CleanedBoard,
+        max_time_sec,
+        heuristic=lambda cleaned_board : cleaned_board.get_garanteed_score() + cleaned_board.get_score() * 0.1
     ):
         """Search game to determine best action; use alpha-beta pruning."""
 
-        def max_value(board, alpha, beta, depth):
-            if (board.is_finished()):
-                return (board.get_score())
+        def max_value(cleaned_board, alpha, beta, depth, max_depth):
+            if (cleaned_board.is_finished()):
+                return (cleaned_board.get_score(), None)
 
-            if (cutoff(board, depth)):
-                return (heuristic(board), None)
+            if (depth >= max_depth):
+                return (heuristic(cleaned_board), None)
             
             best_value = - math.inf
             best_action = None
-            for action in board.get_actions():
-                new_board = board.clone()
-                child_value = min_value(new_board, alpha, beta, depth+1)[0]
+            for action in cleaned_board.get_actions():
+                new_board = cleaned_board.clone()
+                child_value = min_value(new_board, alpha, beta, depth+1, max_depth)[0]
                 if (child_value > best_value):
                     best_value = child_value
                     best_action = action
@@ -142,18 +146,18 @@ class MyAgent(Agent):
                         break
             return (best_value, best_action)
 
-        def min_value(board, alpha, beta, depth):
-            if (board.is_finished()):
-                return (board.get_score())
+        def min_value(cleaned_board, alpha, beta, depth, max_depth):
+            if (cleaned_board.is_finished()):
+                return (cleaned_board.get_score(), None)
 
-            if (cutoff(board, depth)):
-                return (heuristic(board), None)
+            if (depth >= max_depth):
+                return (heuristic(cleaned_board), None)
             
             best_value = math.inf
             best_action = None
-            for action in board.get_actions():
-                new_board = board.clone()
-                child_value = max_value(new_board, alpha, beta, depth+1)[0]
+            for action in cleaned_board.get_actions():
+                new_board = cleaned_board.clone()
+                child_value = max_value(new_board, alpha, beta, depth+1, max_depth)[0]
                 if (child_value < best_value):
                     best_value = child_value
                     best_action = action
@@ -162,7 +166,20 @@ class MyAgent(Agent):
                         break
             return (best_value, best_action)
 
-        return max_value(board, -math.inf, math.inf, 0)
+        init_time = time.time()
+        chosen_action = None
+        explored_depth = 2
+
+        print("##")
+        
+        while ((time.time() - init_time < max_time_sec) and (explored_depth <= 3)):
+            explored_depth += 1
+
+            best_value, chosen_action = max_value(cleaned_board, -math.inf, math.inf, 0, explored_depth)
+            print("Depth : ", explored_depth, ", Best value : ", best_value, ", in ", time.time() - init_time, " sec")
+
+        print("##")
+        return chosen_action
 
 if __name__ == "__main__":
     agent_main(MyAgent())
