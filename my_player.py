fbf1b527d672c14882be399767effe20a8e28ffd
Gaspard Cornu-Emieux <gaspard.ce3@gmail.com>
2022-11-15 16:45:35 -0500

    feat(ab) : Reverted back to classic boards. Implemented memoization (useless without the cleaned boards for now)

--- my_player.py
+++ my_player.py
@@ -21,6 +21,10 @@ from avalam import *
 import math
 import time
 
+BOARD_MEMORY_MAX_DEPTH = 0
+BOARD_MEMORY_HEURISTICS = 1
+BOARD_MEMORY_BEST_ACTION = 2
+
 class CleanedBoard():
     def __init__(self, board : Board):
         self._board = board
@@ -52,12 +56,6 @@ class CleanedBoard():
     def get_cleaned_m(self):
         return self._cleaned_m
 
-    def are_similar(self, other):
-        return any([symetry(other.get_cleaned_m()) == self._cleaned_m for symetry in self._symetry_functions])
-
-    def are_invert_similar(self, other):
-        return any([self._invert(symetry(other.get_cleaned_m())) == self._cleaned_m for symetry in self._symetry_functions])
-    
     def get_garanteed_score(self):
         return self._garanteed_score
 
@@ -74,11 +72,40 @@ class CleanedBoard():
         for i, j, h in self._board.get_towers():
             for action in self._board.get_tower_actions(i, j):
                 yield action
+    
+    def get_similar_boards_m(self):
+        for s in self._symetry_functions:
+            yield s(self._cleaned_m)
+
+class CleanedBoardMemory():
+    def __init__(self):
+        self._dictionary = {}
+    
+    def add_board(self, cleaned_board : CleanedBoard, depth, additionnal_score):
+        cleaned_m = cleaned_board.get_cleaned_m()
+        if ((not cleaned_m in self._dictionary) 
+         or ((cleaned_m in self._dictionary) and (depth > self._dictionary[cleaned_m][0]))):
+            self._dictionary[cleaned_m] = (depth, additionnal_score)
+    
+    def get_board(self, cleaned_board : CleanedBoard, depth):
+        for cleaned_m in cleaned_board.get_similar_boards_m():
+            if ((cleaned_m in self._dictionary) and (depth <= self._dictionary[cleaned_m][0])):
+                return True, self._dictionary[cleaned_m][1]
+        return False, 0
 
 class MyAgent(Agent):
 
     """My Avalam agent."""
-    
+    def __init__(self):
+        self._board_memory_max = {}
+        self._board_memory_min = {}
+        self._stats_memoized_min_max_steps = 0
+        self._stats_total_min_max_steps = 0
+        self._stats_turn_duration_sec = 0
+        self._stats_memory_nb_boards_min = 0
+        self._stats_memory_nb_boards_max = 0
+
+###################################################################################################
     def initialize(self, percepts, players, time_left):
         """Begin a new game.
         The computation done here also counts in the time credit.
@@ -91,7 +118,8 @@ class MyAgent(Agent):
             not time-limited, time_left is None.
         """
         pass
-
+    
+###################################################################################################
     def play(self, percepts, player, step, time_left):
         """
         This function is used to play a move according
@@ -107,80 +135,134 @@ class MyAgent(Agent):
         :return: an action
             eg; (1, 4, 1 , 3) to move tower on cell (1,4) to cell (1,3)
         """
-        print("step:", step)
-        print("time left:", time_left if time_left else '+inf')
-
+        # Get the board and revert it if the agent controls the second player
         board = dict_to_board(percepts)
         board.get_percepts((player == -1))
-        cleaned_board = CleanedBoard(board)
 
-        action = self.h_alphabeta_search(cleaned_board, 5)
-        print("Action played:", action)
+        # Reset the statistics
+        self._stats_memoized_min_max_steps = 0
+        self._stats_total_min_max_steps = 0
+        self._stats_turn_duration_sec = 0
+
+        # Compute the turn
+        init_time = time.time()
+        action = self.progressive_alpha_beta_search(board, 5)
+        self._stats_turn_duration_sec = time.time() - init_time
+
+        # Compute statistics
+        self._stats_memory_nb_boards_min = len(self._board_memory_min)
+        self._stats_memory_nb_boards_max = len(self._board_memory_max)
+
+        # Display stats
+        print(f"Memoisation efficiency : {self._stats_memoized_min_max_steps} / {self._stats_total_min_max_steps}")
+        print(f"Board stored : max : {self._stats_memory_nb_boards_max}, min : {self._stats_memory_nb_boards_min}")
+        print(f"Turn duration : {self._stats_turn_duration_sec} s")
+
         return action
 
-    def h_alphabeta_search(
+###################################################################################################
+    def alpha_beta_search(
         self,
-        cleaned_board : CleanedBoard,
-        max_time_sec,
-        heuristic=lambda cleaned_board : cleaned_board.get_garanteed_score() + cleaned_board.get_score() * 0.1
+        board : Board,
+        max_depth : int,
+        heuristic=lambda board : board.get_score()
     ):
         """Search game to determine best action; use alpha-beta pruning."""
 
-        def max_value(cleaned_board, alpha, beta, depth, max_depth):
-            if (cleaned_board.is_finished()):
-                return (cleaned_board.get_score(), None)
+        def max_value(board, alpha, beta, remaining_depth):        
 
-            if (depth >= max_depth):
-                return (heuristic(cleaned_board), None)
+            self._stats_total_min_max_steps += 1
+
+            # Finishing cases
+            if (board.is_finished()):
+                return (board.get_score(), None)
+
+            if (remaining_depth < 0):
+                return (heuristic(board), None)
             
+            # If the board is already in memory, with a depth searched at least equal to the
+            # one we're looking for, we take it
+            if board in self._board_memory_max:
+                if self._board_memory_max[board][BOARD_MEMORY_MAX_DEPTH] >= remaining_depth:
+                    self._stats_memoized_min_max_steps += 1
+                    return (self._board_memory_max[board][BOARD_MEMORY_HEURISTICS], self._board_memory_max[board][BOARD_MEMORY_BEST_ACTION])
+            
+            # Else, we compute it as for a normal alpha beta
             best_value = - math.inf
             best_action = None
-            for action in cleaned_board.get_actions():
-                new_board = cleaned_board.clone()
-                child_value = min_value(new_board, alpha, beta, depth+1, max_depth)[0]
+            for action in board.get_actions():
+                new_board = board.clone()
+                child_value = min_value(new_board, alpha, beta, remaining_depth - 1)[0]
                 if (child_value > best_value):
                     best_value = child_value
                     best_action = action
                     alpha = max(alpha, best_value)
                     if (alpha >= beta):
                         break
+            
+            # Once computed, the result is stored in memory
+            self._board_memory_max[board] = (remaining_depth, best_value, best_action)
+
             return (best_value, best_action)
 
-        def min_value(cleaned_board, alpha, beta, depth, max_depth):
-            if (cleaned_board.is_finished()):
-                return (cleaned_board.get_score(), None)
-
-            if (depth >= max_depth):
-                return (heuristic(cleaned_board), None)
+        def min_value(board, alpha, beta, remaining_depth):
             
+            self._stats_total_min_max_steps += 1
+
+            # Finishing cases
+            if (board.is_finished()):
+                return (board.get_score(), None)
+
+            if (remaining_depth < 0):
+                return (heuristic(board), None)
+
+            # If the board is already in memory, with a depth searched at least equal to the
+            # one we're looking for, we take it
+            if board in self._board_memory_min:
+                if self._board_memory_min[board][BOARD_MEMORY_MAX_DEPTH] >= remaining_depth:
+                    self._stats_memoized_min_max_steps += 1
+                    return (self._board_memory_min[board][BOARD_MEMORY_HEURISTICS], self._board_memory_min[board][BOARD_MEMORY_BEST_ACTION])
+            
+            # Else, we compute it as for a normal alpha beta
             best_value = math.inf
             best_action = None
-            for action in cleaned_board.get_actions():
-                new_board = cleaned_board.clone()
-                child_value = max_value(new_board, alpha, beta, depth+1, max_depth)[0]
+
+            for action in board.get_actions():
+                # TODO : Order the actions by pertinence, for instance by rising up the actions near the movement
+                new_board = board.clone()
+                child_value = max_value(new_board, alpha, beta, remaining_depth - 1)[0]
                 if (child_value < best_value):
                     best_value = child_value
                     best_action = action
                     beta = min(beta, best_value)
                     if (alpha >= beta):
                         break
+            
+            # Once computed, the result is stored in memory
+            self._board_memory_min[board] = (remaining_depth, best_value, best_action)
+
             return (best_value, best_action)
 
+        return max_value(board, -math.inf, math.inf, max_depth)
+
+###################################################################################################
+    def progressive_alpha_beta_search(
+        self,
+        board : Board,
+        max_time_sec : float,
+        heuristic=lambda board : board.get_score()
+    ):
         init_time = time.time()
         chosen_action = None
-        explored_depth = 2
+        explored_depth = 1
 
-        print("##")
-        
-        while ((time.time() - init_time < max_time_sec) and (explored_depth <= 3)):
+        while ((time.time() - init_time < max_time_sec) and (explored_depth <= 2)):
             explored_depth += 1
-
-            best_value, chosen_action = max_value(cleaned_board, -math.inf, math.inf, 0, explored_depth)
-            print("Depth : ", explored_depth, ", Best value : ", best_value, ", in ", time.time() - init_time, " sec")
-
-        print("##")
+            print(f"Start thinking on action {explored_depth}")
+            chosen_action = self.alpha_beta_search(board, explored_depth, heuristic)[1]
         return chosen_action
 
+###################################################################################################
 if __name__ == "__main__":
     agent_main(MyAgent())
 
